25fced152b6af12cac5c89448ffd04e005d25a4f
Lucas Silva <lucassilva@Lucass-MacBook-Air.local>
2024-05-19 18:21:09 -0300

    :sparkles: add infinite scroll

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,35 @@
+"use client";
+
+import { fetchAnime } from "@/server/actions/anime";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+let page = 2;
 
 function LoadMore() {
+  const [data, setData] = useState<AnimeProp[]>([]);
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData((prevData) => [...prevData, ...res]);
+        page++;
+      });
+    }
+  }, [inView]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
